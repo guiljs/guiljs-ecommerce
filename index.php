4764d158985f38aed6192cc880eb8bef8a937619
Guilherme de Jesus Santos <guilhermejs@hotmail.com>
2018-05-30 20:15:41 -0300

    Parte de Admin. Tpl criado.

--- index.php
+++ index.php
@@ -4,14 +4,21 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim(); //Rotas
 
 $app->config('debug', true);
 
-$app->get('/', function() { //Quando chamar via get a pasta raiz , execute isso 
+$app->get('/', function() { //Quando chamar via get a pasta raiz , execute isso
 
   $page = new Page(); //O construct vai criar o header
+  $page->setTpl("index"); // Carrega o conteúdo
+}); //O destruct do Hcode\Page vai criar o footer
+
+
+$app->get('/admin', function() { //Quando chamar via get a pasta raiz , execute isso
+  $page = new PageAdmin(); //O construct vai criar o header
   $page->setTpl("index"); // Carrega o conteúdo
 }); //O destruct do Hcode\Page vai criar o footer
 
